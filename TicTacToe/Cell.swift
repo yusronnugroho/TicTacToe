7eff598fcc7ee882f7ee945c40a0ad145608a7a8
yusronnugroho <yusronx2smaboy@gmail.com>
2023-06-27 20:54:39 +0700

    tic tac toe

---
+++ TicTacToe/Cell.swift
@@ -0,0 +1,36 @@
+import Foundation
+import SwiftUI
+import UIKit
+
+struct Cell {
+    var tile: Tile
+    
+    func displayTile() -> String {
+        switch(tile){
+            case Tile.Nought:
+                return "O"
+            case Tile.Cross:
+                return "X"
+            default:
+                return ""
+        }
+    }
+    
+    func tileColor() -> Color {
+        switch(tile){
+            case Tile.Nought:
+                return Color.red
+            case Tile.Cross:
+                return Color.black
+            default:
+                return Color.black
+        }
+    }
+}
+
+enum Tile {
+    case Nought
+    case Cross
+    case Empty
+}
+
